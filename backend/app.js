bae2ac7c0f99fc6379620cb6db865f4993009a58
Jean-Sebastien Rakotonirina <78790620+jsr029@users.noreply.github.com>
2024-12-15 10:38:52 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,14 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-/** Premier middleware et sera appliqué à toutes les routes, middleware général */
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
 app.use(express.json());
 
 app.use('/uploads', express.static('uploads'));
@@ -26,23 +27,17 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to mongoDB'))
   .catch((error) => console.log('Not connected to mongoDB', error));
 
-// General error handling middleware for handling duplicate key errors
-app.use((err, req, res, next) => {
-  if (err.code === 11000) {
-    res.status(409).json({ error: 'Duplicate key error' });
-  } else {
-    next(err);
-  }
-});
-
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
 
-// General error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
+  if (err.code === 11000) {
+    res.status(409).json({ error: 'Duplicate key error' });
+  } else {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+  }
 });
 
 module.exports = app;
