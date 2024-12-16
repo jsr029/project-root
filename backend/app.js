e054f9ffa72598114e91326eb32304872b57c81f
jsr029 <jsr059@gmail.com>
2024-12-16 09:21:07 +0100

    ok

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,9 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
-/** Premier middleware et sera appliqué à toutes les routes, middleware général */
 app.use(cors());
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,9 +26,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
 
 app.use('/uploads', express.static('uploads'));
+
+mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to mongoDB'))
