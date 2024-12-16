e4d8b33a81686e826c3adb272cfaf2886a030eb2
jsr029 <jsr059@gmail.com>
2024-12-16 11:00:59 +0100

    e

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.json());
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
