313e3af9a9b60a2a19415f06d29a34dd4d405884
jsr029 <jsr059@gmail.com>
2024-12-16 10:59:35 +0100

    cors

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://project-root-frontend.vercel.app',
-  credentials: true
-}));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use(express.json());
 
